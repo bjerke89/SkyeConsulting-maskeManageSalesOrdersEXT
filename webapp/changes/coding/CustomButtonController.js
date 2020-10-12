7af3e791b6720aa41e5a94a13c397c44fb2a6d96
Nicholas Bjerke <bjerke89@me.com>
2020-10-12 11:09:04 +0000

    New button create sales order

---
+++ webapp/changes/coding/CustomButtonController.js
@@ -0,0 +1,75 @@
+/***
+@controller Name:sap.suite.ui.generic.template.ListReport.view.ListReport,
+*@viewId:cus.sd.salesorders.manage::sap.suite.ui.generic.template.ListReport.view.ListReport::C_SalesOrderWl_F1873
+*/
+sap.ui.define([
+		'sap/ui/core/mvc/ControllerExtension'
+	],
+	function (
+		ControllerExtension
+	) {
+		"use strict";
+		return ControllerExtension.extend("customer.ManageSalesOrdersEXT1.CustomButtonController", {
+			
+			
+			//Navigate to va01 - create sales order without params
+			onCreateSalesOrder: function (oEvent) {
+				var oCrossAppNavigator = sap.ushell.Container.getService("CrossApplicationNavigation");
+				
+				var hash = (oCrossAppNavigator && oCrossAppNavigator.hrefForExternal({
+						target: {
+							semanticObject: "SalesDocument",
+							action: "create"
+						}
+					})) || "";
+
+					oCrossAppNavigator.toExternal({
+						target: {
+							shellHash: hash
+						}
+					});
+			},
+			
+			//Navigate to VA05 tcode
+			onDispSalesOrders: function (oEvent) {
+				var oSelectedItem = this.getView().byId("responsiveTable").getSelectedItem();
+				var oCrossAppNavigator = sap.ushell.Container.getService("CrossApplicationNavigation");
+				
+				//Navigates to search list because no item is selected
+				if (oSelectedItem === null) {
+					var hashNoSelect = (oCrossAppNavigator && oCrossAppNavigator.hrefForExternal({
+						target: {
+							semanticObject: "SalesOrder",
+							action: "list"
+						}
+					})) || "";
+
+					oCrossAppNavigator.toExternal({
+						target: {
+							shellHash: hashNoSelect
+						}
+					});
+				} else {
+					var sPath = this.getView().byId("responsiveTable").getSelectedItem().getBindingContext().sPath;
+					var oModel = oEvent.getSource().getModel();
+					var sSoldToParty = oModel.getData(sPath).ShipToParty;
+					
+					//Navigates to search results with selected Sold-To Party number 
+					var hashSelect = (oCrossAppNavigator && oCrossAppNavigator.hrefForExternal({
+						target: {
+							semanticObject: "SalesOrder",
+							action: "list"
+						},
+						params: {
+							"SoldToParty": sSoldToParty
+						}
+					})) || "";
+					oCrossAppNavigator.toExternal({
+						target: {
+							shellHash: hashSelect
+						}
+					});
+				}
+			}
+		});
+	});
